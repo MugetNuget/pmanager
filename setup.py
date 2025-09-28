b1194fded6cb083591c42e73263ac228dbd09ef5
Santiago Caceres <santiagocaceresv@example.com>
2025-09-28 13:39:06 -0500

    Nueva version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pmanager",
-    version="0.1.7",
+    version="0.2.0",
     packages=find_packages(),
     include_package_data=True,  # <- permite incluir archivos extras
     entry_points={
